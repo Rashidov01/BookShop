5a848f3c88443bfd6ffb6be7457e83b0f51beb21
Abdulloh <72493727+Rashidov01@users.noreply.github.com>
2021-09-23 09:27:53 +0500

    all done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
-var elInput = document.querySelector(".js-input");
-var elOutput = document.querySelector(".js-output");
-var elSubmitBtn = document.querySelector(".js-submit");
-var elForm = document.querySelector(".js-form");
+let elBooksList = document.querySelector('.books__list');
 
-elForm.addEventListener("submit", function (e) {
-  e.preventDefault();
+// books
+let foundBooks = books.slice(0, 50);
 
-})
+// template
+elBooksItemTemplate = document.querySelector('#books-item-template').content;
+
+
+function showBooks () {
+  elBooksList.innerHTML = '';
+  const elBooksFragment = document.createDocumentFragment();
+
+
+  for (let book of books) {
+    const elNewBooksItem = elBooksItemTemplate.cloneNode(true);
+
+    // elNewBooksItem.querySelector('.book__img').src = imgLink;
+    // elNewBooksItem.querySelector('.book__img').alt = `${book.title} poster`;
+    elNewBooksItem.querySelector('.book__title').textContent = book.title;
+    elNewBooksItem.querySelector('.book__author').textContent = book.author;
+    elNewBooksItem.querySelector('.book__year').textContent = book.year;
+    elNewBooksItem.querySelector('.book__pages').textContent = book.pages;
+    elNewBooksItem.querySelector('.book__language').textContent = book.language;
+
+
+    elBooksFragment.appendChild(elNewBooksItem);
+  }
+
+  elBooksList.appendChild(elBooksFragment);
+}
+
+showBooks(foundBooks, '');
